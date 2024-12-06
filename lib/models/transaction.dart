ce79e02ac2279603a6946a78ad7de9e424293e8c
pauloRodri29 <rodriguesjp29@gmail.com>
2024-12-06 13:19:47 -0300

    first version

---
+++ lib/models/transaction.dart
@@ -0,0 +1,8 @@
+class Transaction{
+  final String id;
+  final String title;
+  final String value;
+  final String date;
+
+  Transaction({required this.id, required this.title, required this.value, required this.date});
+}
